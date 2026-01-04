63ee5875fdb830617f5365dcbb56bb04e3e2b4b1
achuzz-code <aswinuday2006@gmail.com>
2026-01-04 14:36:55 +0530

    test

--- backend/audio.hpp
+++ backend/audio.hpp
@@ -1,3 +1,8 @@
+
+#include "portaudio.h"
+#include <math.h>
+#include <stdio.h>
+
 struct dataPoint {
   float frequency;
   float amplitude;
@@ -11,6 +16,14 @@ public:
   audioHandler &operator=(audioHandler &&) = default;
   audioHandler &operator=(const audioHandler &) = default;
   ~audioHandler() = default;
+
+  int audioInputHandlerCallback(const void *inputBuffer, void *outputBuffer,
+                                unsigned long framesPerBuffer,
+                                const PaStreamCallbackTimeInfo *timeInfo,
+                                PaStreamCallbackFlags statusFlags,
+                                void *userData) {
+    return 0;
+  }
 
 private:
 };
